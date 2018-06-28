17faad72db2eb5e6d2e95aea1f8abd1071d05675
Himanshu Thakur <contact@himtuna.com>
2018-06-28 21:19:58 +0530

    Creating Models, Migrations, Controllers and Factories

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+Use App\Model\Category;
+Use App\Model\Question;
+Use App\Model\Reply;
+Use App\Model\Like;
+
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +19,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(User::class,10)->create();
+        factory(Category::class,5)->create();
+        factory(Question::class,10)->create();
+        factory(Reply::class,50)->create()->each(function($reply){
+            return $reply->like()->save(factory(Like::class)->make());
+        });
     }
 }
