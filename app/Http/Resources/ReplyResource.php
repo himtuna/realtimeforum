db3841b9bd44f3a69de2649686802b8878405057
Himanshu Thakur <contact@himtuna.com>
2018-07-01 15:38:20 +0530

    CRUD for Reply

--- app/Http/Resources/ReplyResource.php
+++ app/Http/Resources/ReplyResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class QuestionResource extends JsonResource
+class ReplyResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -16,12 +16,11 @@ class QuestionResource extends JsonResource
     {
         // return parent::toArray($request);
         return [
-            // 'id' => $this->id,
-            'title' => $this->title,
-            'path' => $this->path,
-            'body' => $this->body,
-            'created_at' => $this->created_at->diffForHumans(),
-            'user' => $this->user->name
+            'id' => $this->id,
+            'reply' => $this->body,
+            'user' => $this->user->name,
+            'created_at' => $this->created_at->diffForHumans()
+
         ];
     }
 }
